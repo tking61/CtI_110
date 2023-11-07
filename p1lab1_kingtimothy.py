8d6a191dbf1d2e453c161b368b367f828b86fca4
tking61 <150169194+tking61@users.noreply.github.com>
2023-11-07 11:22:12 -0500

    first upload

---
+++ p1lab1_kingtimothy.py
@@ -0,0 +1,9 @@
+#Timothy King
+#11/7/2023
+#Basic Input/Output
+
+#Get input from user
+name = input("Please emter your name: ")
+
+#Display output to user
+print("Hello", name, "welcome to CTI-110!")
